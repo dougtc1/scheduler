34001c5b4ea3c217039df8d20aa49b2b6a868ccc
Douglas Torres <douglastorres.usb@gmail.com>
2020-11-01 01:11:22 -0600

    Added models to Django admin

--- scheduler/appointments/admin.py
+++ scheduler/appointments/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Appointment, MeetingRoom, Amenity, Appointment_Participant
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+# Needed for inline capability in Sensor model
+class Appointment_ParticipantInline(admin.TabularInline):
+    model = Appointment_Participant
+    can_delete = False
+    verbose_name_plural = 'Participants'
+
+class AppointmentAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+    list_display = (
+        'id',
+        'subject',
+        'start_time',
+        'end_time',
+        'location',
+        'deleted_at',
+        )
+    inlines = [Appointment_ParticipantInline]
+
+class MeetingRoomAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+    list_display = (
+        'id',
+        'name',
+        'maximum_occupancy',
+        'cost_per_hour',
+        )
+
+class AmenityAdmin(admin.ModelAdmin):
+    readonly_fields = ['id']
+    list_display = (
+        'id',
+        'name',
+        'description',
+    )
+
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(MeetingRoom, MeetingRoomAdmin)
+admin.site.register(Amenity, AmenityAdmin)
